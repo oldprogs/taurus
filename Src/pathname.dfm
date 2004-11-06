99ec1935bed844ae8dc7b2b2cddac6495cf606bd
taurus <taurus>
2004-11-06 14:36:18 +0000

    no message

--- Src/pathname.dfm
+++ Src/pathname.dfm
@@ -5,8 +5,8 @@ object PathNamesForm: TPathNamesForm
   BorderStyle = bsDialog
   BorderWidth = 6
   Caption = 'Path Names'
-  ClientHeight = 249
-  ClientWidth = 448
+  ClientHeight = 241
+  ClientWidth = 442
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -20,13 +20,13 @@ object PathNamesForm: TPathNamesForm
   OnClose = FormClose
   OnCreate = FormCreate
   DesignSize = (
-    448
-    249)
+    442
+    241)
   PixelsPerInch = 96
   TextHeight = 14
   object llDefZone: TLabel
-    Left = 172
-    Top = 189
+    Left = 166
+    Top = 181
     Width = 48
     Height = 14
     Alignment = taRightJustify
@@ -38,6 +38,8 @@ object PathNamesForm: TPathNamesForm
     Top = 3
     Width = 424
     Height = 158
+    FileNameCol = 1
+    FileNameDir = True
     FixedFont.Charset = DEFAULT_CHARSET
     FixedFont.Color = clWindowText
     FixedFont.Height = -12
@@ -55,13 +57,14 @@ object PathNamesForm: TPathNamesForm
     Options = [goFixedVertLine, goFixedHorzLine, goVertLine, goHorzLine, goEditing, goFixedNumCols]
     ParentFont = False
     TabOrder = 0
+    CheckBoxes = False
     ColWidths = (
-      107
+      120
       281)
   end
   object bBrowse: TButton
-    Left = 356
-    Top = 185
+    Left = 350
+    Top = 177
     Width = 75
     Height = 23
     Anchors = [akRight, akBottom]
@@ -70,8 +73,8 @@ object PathNamesForm: TPathNamesForm
     OnClick = bBrowseClick
   end
   object bOK: TButton
-    Left = 196
-    Top = 219
+    Left = 190
+    Top = 211
     Width = 75
     Height = 23
     Anchors = [akRight, akBottom]
@@ -81,8 +84,8 @@ object PathNamesForm: TPathNamesForm
     TabOrder = 2
   end
   object bCancel: TButton
-    Left = 276
-    Top = 219
+    Left = 270
+    Top = 211
     Width = 75
     Height = 23
     Anchors = [akRight, akBottom]
@@ -92,8 +95,8 @@ object PathNamesForm: TPathNamesForm
     TabOrder = 3
   end
   object bHelp: TButton
-    Left = 356
-    Top = 219
+    Left = 350
+    Top = 211
     Width = 75
     Height = 23
     Anchors = [akRight, akBottom]
@@ -102,100 +105,21 @@ object PathNamesForm: TPathNamesForm
     OnClick = bHelpClick
   end
   object Edit1: TEdit
-    Left = 232
-    Top = 185
+    Left = 226
+    Top = 177
     Width = 121
     Height = 22
     Anchors = [akRight, akBottom]
     TabOrder = 5
     Text = '2:2/0'
   end
-  object Button1: TButton
-    Left = 400
-    Top = 5
-    Width = 31
-    Height = 20
-    Caption = '...'
-    TabOrder = 6
-    OnClick = Button1Click
-  end
-  object Button2: TButton
-    Tag = 1
-    Left = 400
-    Top = 24
-    Width = 31
-    Height = 20
-    Caption = '...'
-    TabOrder = 7
-    OnClick = Button1Click
-  end
-  object Button3: TButton
-    Tag = 2
-    Left = 400
-    Top = 43
-    Width = 31
-    Height = 20
-    Caption = '...'
-    TabOrder = 8
-    OnClick = Button1Click
-  end
-  object Button4: TButton
-    Tag = 3
-    Left = 400
-    Top = 62
-    Width = 31
-    Height = 20
-    Caption = '...'
-    TabOrder = 9
-    OnClick = Button1Click
-  end
-  object Button5: TButton
-    Tag = 4
-    Left = 400
-    Top = 81
-    Width = 31
-    Height = 20
-    Caption = '...'
-    TabOrder = 10
-    OnClick = Button1Click
-  end
-  object Button6: TButton
-    Tag = 5
-    Left = 400
-    Top = 100
-    Width = 31
-    Height = 20
-    Caption = '...'
-    TabOrder = 11
-    OnClick = Button1Click
-  end
-  object Button7: TButton
-    Tag = 6
-    Left = 400
-    Top = 119
-    Width = 31
-    Height = 20
-    Caption = '...'
-    TabOrder = 12
-    OnClick = Button1Click
-  end
-  object Button8: TButton
-    Tag = 7
-    Left = 400
-    Top = 138
-    Width = 31
-    Height = 20
-    Caption = '...'
-    TabOrder = 13
-    OnClick = Button1Click
-  end
   object CB: TCheckBox
-    Left = 9
-    Top = 189
+    Left = 3
+    Top = 181
     Width = 159
     Height = 14
     Anchors = [akRight, akBottom]
     Caption = '5D outbound support'
-    TabOrder = 14
+    TabOrder = 6
   end
 end
--- Src/pathname.pas
+++ Src/pathname.pas
@@ -18,22 +18,12 @@ type
     bHelp: TButton;
     llDefZone: TLabel;
     Edit1: TEdit;
-    Button1: TButton;
-    Button2: TButton;
-    Button3: TButton;
-    Button4: TButton;
-    Button5: TButton;
-    Button6: TButton;
-    Button7: TButton;
-    Button8: TButton;
     CB: TCheckBox;
     procedure FormActivate(Sender: TObject);
-    procedure bChangeHomeDirClick(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure bHelpClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure bBrowseClick(Sender: TObject);
-    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
     Activated: Boolean;
@@ -54,24 +44,6 @@ xBase, AltRecs, Recs, LngTools, ShellApi, PathNmEx, RadIni, FileCtrl, xFido, Adr
 
 var FidoAddr: TFidoAddress;
 
-procedure TPathNamesForm.bChangeHomeDirClick(Sender: TObject);
-var
-  s: string;
-begin
-//  s:=Browse(Self.Handle, lHomeDir.Caption);
-  if s = '' then Exit;
-//  if not SetRegHomeDir(s) then begin DisplayErrorLng(rsCantUpdateReg, Handle); Exit end;
-  IniFile.HomeDir:=S;
-//  lHomeDir.Caption:=s;
-  if UpperCase(MakeNormName(s, '')) = UpperCase(MakeNormName(HomeDir, '')) then Exit;
-  { And now exit! }
-  UpdateConfig;
-  DisplayInfoLng(rsPNHdc, Handle);
-  ShellExecute(0,nil,PChar(ParamStr(0)),
-        PChar('delay2000'),PChar(ExtractFilePath(ParamStr(0))),sw_shownormal);
-  PostCloseMessage;
-end;
-
 procedure TPathNamesForm.FormActivate(Sender: TObject);
 begin
   if not Activated then
@@ -83,16 +55,15 @@ end;
 
 procedure TPathNamesForm.SetData;
 begin
-  Edit1.Text:=Addr2Str(IniFile.MainAddr);
-//  ZoneSpin.Value := IniFile.MainAddr.Zone;
-  gSpec.Cells[1,0]:=IniFile.HomeDir;
-  gSpec.Cells[1,1]:=IniFile.CfgDir;
-  gSpec.Cells[1,2]:=IniFile.InSecure;
-  gSpec.Cells[1,3]:=IniFile.InCommon;
-  gSpec.Cells[1,4]:=IniFile.InTemp;
-  gSpec.Cells[1,5]:=IniFile.Outbound;
-  gSpec.Cells[1,6]:=IniFile.Log;
-  gSpec.Cells[1,7]:=IniFile.FlagsDir;
+  Edit1.Text := Addr2Str(IniFile.MainAddr);
+  gSpec.Cells[1, 0] := IniFile.HomeDir;
+  gSpec.Cells[1, 1] := IniFile.CfgDir;
+  gSpec.Cells[1, 2] := IniFile.InSecure;
+  gSpec.Cells[1, 3] := IniFile.InCommon;
+  gSpec.Cells[1, 4] := IniFile.InTemp;
+  gSpec.Cells[1, 5] := IniFile.Outbound;
+  gSpec.Cells[1, 6] := IniFile.Log;
+  gSpec.Cells[1, 7] := IniFile.FlagsDir;
   CB.Checked := IniFile.D5out;
 end;
 
@@ -110,32 +81,28 @@ procedure TPathNamesForm.UpdateConfig;
 begin
   IniFile.D5out := CB.Checked;
   AltStoreConfig(Handle);
-  IniFile.InSecure:=gSpec.Cells[1,2];
-  IniFile.InCommon:=gSpec.Cells[1,3];
-  IniFile.InTemp:=gSpec.Cells[1,4];
-  IniFile.Outbound:=gSpec.Cells[1,5];
-  IniFile.Log:=gSpec.Cells[1,6];
-  IniFile.FlagsDir:=gSpec.Cells[1,7];
-//  IniFile.DefaultZone := ZoneSpin.Value;
+  IniFile.InSecure := gSpec.Cells[1, 2];
+  IniFile.InCommon := gSpec.Cells[1, 3];
+  IniFile.InTemp := gSpec.Cells[1, 4];
+  IniFile.Outbound := gSpec.Cells[1, 5];
+  IniFile.Log := gSpec.Cells[1, 6];
+  IniFile.FlagsDir := gSpec.Cells[1, 7];
   IniFile.MainAddr := FidoAddr;
-  if IniFile.HomeDir<>gSpec.Cells[1,0] then
-  begin
-    IniFile.HomeDir:=gSpec.Cells[1,0];
-    IniFile.CfgDir:=gSpec.Cells[1,1];
+  if IniFile.HomeDir <> gSpec.Cells[1, 0] then begin
+    IniFile.HomeDir := gSpec.Cells[1, 0];
+    IniFile.CfgDir := gSpec.Cells[1, 1];
     If not DirectoryExists(IniFile.HomeDir) then CreateDir(IniFile.HomeDir);
     DisplayInfoLng(rsPNHdc, Handle);
     ShellExecute(0,nil,PChar(ParamStr(0)),
-        PChar('delay5000'),PChar(ExtractFilePath(ParamStr(0))),sw_shownormal);
+        PChar('delay5000'), PChar(ExtractFilePath(ParamStr(0))), sw_shownormal);
     PostCloseMessage;
     exit
   end;
-  if IniFile.CfgDir<>gSpec.Cells[1,1]then
-  begin
-//    IniFile.RadHomeDir:=gSpec.Cells[1,0];
-    IniFile.CfgDir:=gSpec.Cells[1,1];
+  if IniFile.CfgDir <> gSpec.Cells[1, 1] then begin
+    IniFile.CfgDir := gSpec.Cells[1, 1];
     DisplayInfoLng(rsCDCh, Handle);
-    ShellExecute(0,nil,PChar(ParamStr(0)),
-        PChar('delay5000'),PChar(ExtractFilePath(ParamStr(0))),sw_shownormal);
+    ShellExecute(0, nil, PChar(ParamStr(0)),
+        PChar('delay5000'), PChar(ExtractFilePath(ParamStr(0))), sw_shownormal);
     PostCloseMessage;
     exit
   end;
@@ -168,14 +135,6 @@ procedure TPathNamesForm.bBrowseClick(Sender: TObject);
 begin
   if InputSingleAddress(LngStr(rsMMBrsNdlAt), FidoAddr, nil) then
     Edit1.Text:=Addr2Str(FidoAddr);
-end;
-
-procedure TPathNamesForm.Button1Click(Sender: TObject);
-var
-  s: string;
-begin
-  s:=Browse(Handle,gSpec.Cells[1,TComponent(Sender).Tag]);
-  if s<>'' then gSpec.Cells[1,TComponent(Sender).Tag]:=s;
 end;
 
 end.
